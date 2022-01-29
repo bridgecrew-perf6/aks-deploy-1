d5a199959a7c7baf75c2f168e097d2f73de2a776
Lev Pasichnyi <lev.pasichnyi@gmail.com>
2022-01-29 13:35:06 +0200

    Update aks-create-deploy.ps1

--- aks-create-deploy.ps1
+++ aks-create-deploy.ps1
@@ -12,6 +12,17 @@ az account set --subscription "my-subscription"
 # print out resource group list of subscription
 az group list -o table
 # ------------------- Create and deploy AKS cluster ------------------ 
+# get supported aks versions
+az aks get-versions --location eastus --output table
+# create resource group for AKS
+az group create -l "West Europe" -n "levpa-aks-test"
 
+[CmdletBinding()]
+param ($name = 'aks29012022', $rgName = 'levpa-aks-test', $nodeCount = 1, $kubernetesVersion = '1.22.4')
 
+az aks create --generate-ssh-keys `
+              --name $name `
+              --resource-group $rgName `
+              --node-count $nodeCount `
+              --kubernetes-version $kubernetesVersion
 
