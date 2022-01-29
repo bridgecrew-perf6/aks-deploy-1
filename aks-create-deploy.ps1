a3aecb4853bf4aca2cb89c8532008f8862bb5f47
Lev Pasichnyi <lev.pasichnyi@gmail.com>
2022-01-29 22:31:31 +0200

    Update aks-create-deploy.ps1

--- aks-create-deploy.ps1
+++ aks-create-deploy.ps1
@@ -26,3 +26,21 @@ az aks create --generate-ssh-keys `
               --node-count $nodeCount `
               --kubernetes-version $kubernetesVersion
 
+# merge creds from Azure AKS to ~\.kube\config
+az aks get-credentials --name aks29012022 --resource-group levpa-aks-test
+
+# test connection to Azure AKS
+kubectl get nodes
+# aks-nodepool1-38167155-vmss000000   Ready    agent   8h    v1.22.4
+
+# deploy manifest to AKS cluster
+kubectl create -f .\nginx.yml
+
+kubectl get deployments
+# NAME               READY   UP-TO-DATE   AVAILABLE   AGE
+# nginx-deployment   2/2     2            2           20s
+
+kubectl get service
+# NAME            TYPE           CLUSTER-IP    EXTERNAL-IP      PORT(S)        AGE
+# kubernetes      ClusterIP      10.0.0.1      <none>           443/TCP        8h
+# nginx-service   LoadBalancer   10.0.39.235   20.103.166.207   80:30920/TCP   2m55s
